e122518e7bb9760d6bc2b9ada3dd7f4927db8832
qaz11807 <qaz11807@gmail.com>
2023-07-04 11:45:08 +0800

    Update files related renamed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,7 +53,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "hahow_side_project_production"
+  # config.active_job.queue_name_prefix = "back_founder_side_project_production"
 
   config.action_mailer.perform_caching = false
 
