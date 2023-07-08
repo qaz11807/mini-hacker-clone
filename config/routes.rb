78642215be2980c3eca246a36dd8551f61463fcc
qaz11807 <qaz11807@gmail.com>
2023-07-08 11:01:08 +0800

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,18 @@ Rails.application.routes.draw do
       namespace :user do
         post '/register', action: 'register'
       end
+
+      resources :posts, module: :posts, controller: :base, only: [:index, :create] do
+        resources :comments, only: [:create]
+      end
+
+      resources :comments, module: :comments, controller: :base, only: [:show] do
+        member do
+          post '/voted', action: 'add'
+        end
+
+        resources :comments, only: [:create]
+      end
     end
   end
 end
